ea6e769d54ff6a682598295a74e18dd81e4f622f
CollieryNarshe <adrieldbradley@gmail.com>
2022-10-08 10:33:26 -0300

    Add options

--- rename.py
+++ rename.py
@@ -3,20 +3,61 @@
 from glob import glob
 import os
 from sys import exit
+from os import path
+from colorama import Fore, init
+init(autoreset=True)
+
+def rename_files(filenames, old_pattern, new_pattern, dots=False):
+    for f in filenames:
+        if old_pattern in f:
+            if dots:
+                filename, extension = path.splitext(f)
+            else:
+                filename = f
+            new = filename.replace(old_pattern, new_pattern)
+            if dots:
+                new = new + extension
+            os.rename(f, new)
+
+
 
 while True:
-
+    # Get and print filenames
     for item in os.listdir():
-        print(item)
+        print(Fore.RED + item)
+    print("\nOptions: dots, between, ?, q")
+    files = glob('*')  # ./*
 
-    files = glob('./*')
-
+    # First input
     pattern = input("Text to change: ")
-    if not pattern:
+    # ? Info
+    if pattern == "?":
+        print(Fore.GREEN + "\ndots: Delete all periods from each filename, except extension.\n"
+              "between: Delete all characters between two given patterns.\n"
+              "q/exit: Quit.\n")
+        input()
+        continue
+    # Exit
+    if not pattern or pattern.lower() == "exit" or pattern.lower() == 'q':
         exit()
+    # Remove all dots except file extension
+    if pattern.lower() == "dots":
+        rename_files(files, '.', '', dots=True)
+        continue
+    # Between: Delete characters between two inputs:
+    if pattern.lower() == 'between':
+        left = input("Left marker: ")
+        right = input("Right marker: ")
+        for file_name in files:
+            if left in file_name and right in file_name:
+                idx1 = file_name.index(left)
+                idx2 = file_name.index(right)
+                new_file_name = file_name[:idx1] + left + file_name[idx2:]
+                print(new_file_name)
+                os.rename(file_name, new_file_name)
+        continue
+
+    # Second input
     replacement = input("Change to: ")
 
-    for f in files:
-        if pattern in f:
-            new = f.replace(pattern, replacement)
-            os.rename(f, new)
+    rename_files(files, pattern, replacement)
